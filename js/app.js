edf2a502ea3a82109561931b52167332ecb44dad
Ivan <ivansanchez_1988@hotmail.com>
2016-04-16 15:17:48 -0500

    Lightbox: Agregada solucion de lightbox a la galeria usando JQuery

---
+++ js/app.js
@@ -0,0 +1,48 @@
+//Problema: Las imagenes llevan a un punto muerto,  eso es mala UX
+//Solucion: Crear una capa extra con imagenes de mayor tamano. Lightbox
+
+//2. Agregar Overlay Lightbox
+var $overlay = $("<div id='overlay'></div>");
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+//2.1 con imagen
+$overlay.append($image);
+
+//2.2 Un texto
+$overlay.append($caption);
+
+//agregar el overlay al body
+$("body").append($overlay);
+
+
+//1. Usuario da clic en un link que rodea la imagen. Capturar el evento
+$("#galeria li a").click(function(event){
+	event.preventDefault();
+	var href = $(this).attr("href");
+
+	//1.1. Mostrar el light box con la imagen a la que se dio click
+	$image.attr("src",href);
+
+	var texto = $(this).children("img").attr("alt");
+	$caption.text(texto);
+
+	$overlay.show();
+	console.log(href);
+});
+
+
+//1.2. Obtener el elemento alt para mostrar el texto.
+
+
+//3. Cuando se da click al overlay
+//3.1 Esconder el ovelay
+$overlay.click(function(){
+	$overlay.hide();
+});
+
+/***********Menu Para Moviles***************/
+//Problema: El menu no funciona tan bien para moviles.
+//Solucion: Cambiar por una navegacion lateral mas util
+
+//Creemos el menu lateral y aguemoslo al menu
